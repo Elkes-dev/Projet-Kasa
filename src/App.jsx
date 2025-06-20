4b1d045f1c671c148ddca969c468d91c3ba64db2
Eytan Elkeslassy <eytan_elkes1@hotmail.fr>
2025-06-20 16:02:22 +0200

    Mise en place de react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
+import {Routes, Route} from 'react-router-dom'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import logement from './logement.json' 
+
 
 function App() {
   const [count, setCount] = useState(0)
-
+console.log( logement)
   return (
     <>
       <div>
+        
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>

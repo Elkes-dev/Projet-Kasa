3581dc4a2ae25274250e71a5e1defd73cddd0e14
Eytan Elkeslassy <eytan_elkes1@hotmail.fr>
2025-07-18 14:20:03 +0200

    Changement Browse router en hashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import {BrowserRouter as Router} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    < Router basename="/Projet-Kasa" > 
-      < App />
-      </Router>
+    <HashRouter basename="/Projet-Kasa">
+  <App />
+</HashRouter>
   </StrictMode>,
 )
